bca0c7eaa329b0d6e7bd76cce8782a7b16c4744b
tduncan14 <nbatre1@yahoo.com>
2023-01-05 13:04:20 -0800

    keep moving forward

--- 1_socket/Chat_clientside.py
+++ 1_socket/Chat_clientside.py
@@ -12,3 +12,29 @@ btyesize=1024
 #create a client socet and connect to the server
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.connect((DEST_IP, DEST_PORT))
+
+
+#Send/Recieve messages
+
+while True:
+    #Recieve information 
+    message = client_socket.recv(btyesize).decode(Encoder)
+    
+    #QUIT if the connected server wants to quit, else keep sending message
+
+
+    if message == "quit":
+       client_socket.send("quit".encode(Encoder))
+       print("\nEnding the chat ... goodbye!")
+       break
+    else:
+        print(f"/n{message}")
+        message = input("Message:")
+        client_socket.send(message.encode(Encoder))
+
+
+
+
+#Close the client socket
+
+client_socket.close()
--- 1_socket/Chat_serverside.py
+++ 1_socket/Chat_serverside.py
@@ -10,6 +10,8 @@ HOST_PORT = 12345
 ENCORDER ="utf-8"
 
 
+btyesize = 1024
+
 #Create a server socket, bind it to an ip/port, and listen
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,4 +28,27 @@ client_socket,client_address = server_socket.accept()
 client_socket.send("You are connected to the server...\n".encode(ENCORDER))
 
 
+
+#send/recieve message
+
+while True:
+     #Reciever inormation from the client
+     message = client_socket.recv(btyesize).decode(ENCORDER)
+
+     #Quit if the client socket wants to quit, else display the message
+
+     if message == "quit":
+        client_socket.send("quit".encode(ENCORDER))
+        print("\nEnding the chat ..goodbye!")
+        break
+     else:
+        print(f"\n{message}")
+        message = input("Message: ")
+        client_socket.send(message.encode(ENCORDER))
+
+
+
+#Close the socket 
+
+server_socket.close()
 
