3ae7b69f562c76f7aa35a4a040ca84b0fdf2814c
tduncan14 <nbatre1@yahoo.com>
2023-01-24 21:34:36 -0800

    keep moving forward

---
+++ pythonLanguage/main.py
@@ -0,0 +1,34 @@
+
+
+Str = "learn Python"
+
+ind = Str[0]
+
+
+print(ind)
+
+
+
+Str = "python"
+print(Str.upper())
+print(len(Str))
+
+
+
+num = 119
+
+list1 = [ 1,2,3,4,5 ,'python']
+
+print(list1[5],'this the 6th index')
+
+
+
+odd = [1,2,3,5]
+evn = [0,2,4,6]
+
+
+num = [ odd, evn]
+
+num.sort()
+
+print(num)
